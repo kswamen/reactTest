6103a6ce0cdef411d3e903840e891ceedeb49098
ChoiYongWook <cyw0803@naver.com>
2020-07-17 11:23:23 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello therer</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = {
+  name: "홍길동",
+  birthday: "951222",
+  gender: "남자",
+  job: "대학생",
+};
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      ></Customer>
+    );
+  }
 }
 
 export default App;
