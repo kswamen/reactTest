6f2393d90efcd947ccb895a229b8c03baf185a0f
kswamen <kswamen@naver.com>
2020-07-17 11:35:55 +0900

    22

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Customer from "./components/Customer";
 import "./App.css";
 
 const customer = {
+  id: 1,
+  image: "https://placeimg.com/64/64/any",
   name: "홍길동",
-  birthday: "951222",
+  birthday: "961222",
   gender: "남자",
   job: "대학생",
 };
@@ -13,11 +15,13 @@ class App extends Component {
   render() {
     return (
       <Customer
+        id={customer.id}
+        image={customer.image}
         name={customer.name}
         birthday={customer.birthday}
         gender={customer.gender}
         job={customer.job}
-      ></Customer>
+      />
     );
   }
 }
